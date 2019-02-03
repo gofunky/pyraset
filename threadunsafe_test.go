571b2ee85d7bcaf651f86e599a7c9e1ef23e0720
matfax <matthias.fax@gmail.com>
2019-02-03 18:41:15 +0100

    test: fix incomplete String test

--- threadunsafe_test.go
+++ threadunsafe_test.go
@@ -9,34 +9,40 @@ func Test_threadUnsafeSet_String(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-		want   string
+		want   []string
 	}{
 		{
 			name:   "empty set",
 			fields: fields{},
-			want:   "Set{}",
+			want:   []string{"Set{}"},
 		},
 		{
 			name:   "unary set",
 			fields: fields{"one"},
-			want:   "Set{one}",
+			want:   []string{"Set{one}"},
 		},
 		{
 			name:   "binary set",
 			fields: fields{"one", "two"},
-			want:   "Set{one, two}",
+			want:   []string{"Set{one, two}", "Set{two, one}"},
 		},
 		{
 			name:   "nested set",
 			fields: fields{"one", NewSet("two")},
-			want:   "Set{one, Set{two}}",
+			want:   []string{"Set{one, Set{two}}", "Set{Set{two}, one}"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			set := NewSet(tt.fields...)
-			if got := set.String(); got != tt.want {
-				t.Errorf("threadUnsafeSet.String() = %v, want %v", got, tt.want)
+			for i, want := range tt.want {
+				got := set.String()
+				if got == want {
+					break
+				}
+				if i == len(tt.want)-1 {
+					t.Errorf("threadUnsafeSet.String() = %v, want %v", got, tt.want)
+				}
 			}
 		})
 	}
